1d273b94c4a6a0282fa58b6af7682f313def702c
Devdutta Biswas <dev.biswas4496@gmail.com>
2025-03-11 21:15:29 -0400

    login status code updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -77,7 +77,7 @@ export const login = async (req, res) => {
                 role: user.role,
             })
         } else {
-            res.status(401).json({ message: "Invalid credentials" });
+            res.status(400).json({ message: "Invalid credentials" });
         }
     } catch (error) {
         console.log("Error logging in user", error.message);
